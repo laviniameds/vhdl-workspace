9e238fa4831b24db7b6add855f361337ebfadd8d
Lavinia Miranda <laviniameds@icloud.com>
2020-05-26 21:28:56 -0300

    and

---
+++ ALU/and/and.vhdl
@@ -0,0 +1,14 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+entity and is
+    port(
+        a, b: in std_logic;
+        c   : out std_logic
+    );
+end and;
+
+architecture behave of and is
+begin
+    c <= a and b;
+end behave;
