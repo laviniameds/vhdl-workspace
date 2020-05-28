edf687be8d74c30fde65748537a826bb658fd1eb
Lavinia Miranda <laviniameds@icloud.com>
2020-05-27 23:06:42 -0300

    cell components

--- ALU/cell/cell.vhdl
+++ ALU/cell/cell.vhdl
@@ -9,9 +9,7 @@ entity cell is
     );
 end cell;
 
-architecture behave of and is
-
-begin
+architecture behave of cell is
     component and is
         port(
             x, y: in std_logic;
@@ -25,4 +23,36 @@ begin
             z   : out std_logic
         );
     end component;
+
+    component xor is
+        port(
+            x, y: in std_logic;
+            z   : out std_logic
+        );
+    end component;
+
+    component not is
+        port(
+            x   : in std_logic;
+            z   : out std_logic
+        );
+    end component;
+
+    component full_adder is 
+        port(
+            x, y, cin: is std_logic;
+            z, cout   : out std_logic
+        );
+    end component;
+
+    component subtractor is 
+    port(
+        x, y, cin: is std_logic;
+        z, cout   : out std_logic
+    );
+    end component;
+
+    begin
+
+
 end behave;
