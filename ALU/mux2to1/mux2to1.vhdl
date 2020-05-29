d3219e4f2677f847a364515583cf5e8892721d6f
Lavinia Miranda <laviniameds@icloud.com>
2020-05-29 15:10:26 -0300

    mux 2 to 1

---
+++ ALU/mux2to1/mux2to1.vhdl
@@ -0,0 +1,15 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+entity MUX2TO1 is
+    port(
+        a, b : in std_logic;
+        sel  : in std_logic;
+        c    : out std_logic
+    );
+end MUX2TO1;
+
+architecture BEHAVE of MUX2TO1 is
+begin
+    c <= a when (sel = '0') else b;
+end BEHAVE;
