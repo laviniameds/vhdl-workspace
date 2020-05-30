3159fbbd208978aa569b52faa70e05e6dc14a38d
Lavinia Miranda <laviniameds@icloud.com>
2020-05-29 22:57:12 -0300

    and with 3 inputs

---
+++ ALU/and/and3.vhdl
@@ -0,0 +1,14 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+entity AND3 is
+    port(
+        a, b, c : in std_logic;
+        d       : out std_logic
+    );
+end AND3;
+
+architecture BEHAVE of AND3 is
+begin
+    d <= a and b and c;
+end BEHAVE;
