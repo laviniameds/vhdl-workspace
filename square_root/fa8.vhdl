8543267d4baefc7e2d78fc5cc98c369e8d1caedf
Lavinia Miranda <laviniameds@icloud.com>
2020-06-25 15:37:47 -0300

    update fa

---
+++ square_root/fa8.vhdl
@@ -0,0 +1,14 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+entity FA8 is
+    port(
+        a, b : in std_logic_vector(7 downto 0);
+        s    : out std_logic_vector(7 downto 0)
+    );
+end FA8;
+
+architecture BEHAVE of FA8 is
+begin
+    s <= a + b;
+end BEHAVE;
