7a40faef24cb081fecef0961d5fff80add971214
Lavinia Miranda <laviniameds@icloud.com>
2020-06-30 16:05:50 -0300

    test reg8

---
+++ square_root/reg8.do
@@ -0,0 +1,14 @@
+vlib work
+vmap work
+
+vcom reg8.vhdl
+
+vsim reg8_tb
+
+add wave data_in_signal
+add wave clk_signal
+add wave data_out_signal
+
+view wave 
+run 100 ns
+wave zoom full
---
+++ square_root/reg8_tb.vhdl
@@ -0,0 +1,57 @@
+-- Test Bench
+LIBRARY IEEE;
+USE ieee.std_logic_1164.ALL;
+
+ENTITY reg8_tb IS
+END reg8_tb;
+
+ARCHITECTURE test OF reg8_tb IS
+    COMPONENT REG8 IS
+    	port(
+		data_in: in std_logic_vector(7 downto 0); 
+		clk: in std_logic;
+		data_out: out std_logic_vector(7 downto 0)
+    	);	
+    END COMPONENT;
+
+    SIGNAL data_in_signal, data_out_signal : std_logic_vector(7 downto 0);
+    SIGNAL clk_signal : std_logic;
+
+BEGIN
+    reg8_sq : REG8 PORT MAP(
+        -- connect ports to signals
+        data_in => data_in_signal,
+	clk => clk_signal,
+	data_out => data_out_signal
+    );
+
+    PROCESS BEGIN
+
+        data_in_signal <= "00000000";
+        clk_signal <= '0';
+        WAIT FOR 1 ns;
+
+        data_in_signal <= "00000011";
+        clk_signal <= '0';
+        WAIT FOR 1 ns;
+
+        data_in_signal <= "00000011";
+        clk_signal <= '1';
+        WAIT FOR 1 ns;
+
+        data_in_signal <= "00000000";
+        clk_signal <= '0';
+        WAIT FOR 1 ns;
+
+        data_in_signal <= "00000000";
+        clk_signal <= '1';
+        WAIT FOR 1 ns;
+
+        data_in_signal <= "00000000";
+        clk_signal <= '0';
+        WAIT FOR 1 ns;
+
+        WAIT;
+
+    END PROCESS;
+END test;
